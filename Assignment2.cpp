22a2418c03380db13ec831da00f55eef50c02117
Eli Hermann <Eli_Hermann@hotmail.com>
2021-09-28 18:10:50 -0600

    uniform distribution is working at least for nice numbers

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 #include <vector>
 
 class DistributionPair
@@ -18,17 +19,40 @@ std::vector<DistributionPair>
 generateUniformDistribution(std::uint32_t howMany, std::uint32_t min,
                             std::uint32_t max, std::uint8_t numberBins)
 {
-    std::vector<DistributionPair> pairs;
-    for (int i = 0; i < howMany; i++)
+    // Generate bins first
+    std::vector<DistributionPair> bins;
+    std::uint32_t binSize = (max - min) / numberBins;
+    std::uint32_t previousMax = 0;
+    for (std::uint32_t i = binSize; i <= max; i += binSize)
     {
-        pairs.push_back(DistributionPair(min, max));
+        bins.push_back(DistributionPair(previousMax, i));
+        previousMax = i;
     }
 
-    for (DistributionPair d : pairs)
+    for (DistributionPair dp : bins)
     {
-        std::cout << d.minValue << std::endl;
+        std::cout << dp.maxValue << std::endl;
     }
-    return pairs;
+    std::cout << std::endl;
+
+    // Then populate bins with random numbers
+    std::random_device rd;
+    std::cout << "Entropy " << rd.entropy() << std::endl;
+    std::mt19937 engine(rd());
+    std::uniform_int_distribution<unsigned int> distInt(0, 100);
+    for (std::uint32_t i = 0; i < howMany; i++)
+    {
+        auto rn = distInt(engine);
+        bins[rn / binSize].count += 1;
+        std::cout << rn << std::endl;
+    }
+    std::cout << std::endl;
+
+    for (DistributionPair dp : bins)
+    {
+        std::cout << dp.count << std::endl;
+    }
+    return bins;
 }
 
 std::vector<DistributionPair>
@@ -38,4 +62,4 @@ std::vector<DistributionPair>
 generatePoissonDistribution(std::uint32_t howMany, std::uint8_t howOften,
                             std::uint8_t numberBins);
 
-int main() { generateUniformDistribution(10, 21, 30, 5); }
+int main() { generateUniformDistribution(5, 0, 100, 5); }
